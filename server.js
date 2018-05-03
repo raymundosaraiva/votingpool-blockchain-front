ae1c12779115b1d4c0192de280b6107c7ff663e4
Raymundo Saraiva <raymundosaraiva@hotmail.com>
2018-05-03 16:05:52 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ var app = express();
 var path = require('path');
 var port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/img'));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
-    res.sendFile(path.join(__dirname + '/semantic/dist/semantic.min.js'));
-    res.sendFile(path.join(__dirname + '/semantic/dist/semantic.min.css'));   
 });
 
 app.listen(port);
