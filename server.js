d9bbb7cd5cbf176bed2d26e12c2b960645745750
Raymundo Saraiva <raymundosaraiva@hotmail.com>
2018-05-03 15:03:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var port = process.env.PORT || 8080;
 
-// viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(8080);
+app.listen(port);
