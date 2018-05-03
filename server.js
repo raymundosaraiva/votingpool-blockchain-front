3909a4a97752b33413f09a69d3ee64d43a559081
Raymundo Saraiva <raymundosaraiva@hotmail.com>
2018-05-03 15:55:57 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ var app = express();
 var path = require('path');
 var port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/img/'));
-app.use(express.static(__dirname + '/semantic/'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
